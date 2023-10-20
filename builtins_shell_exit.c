f3a7b7ee02b35f04e45fca53b05faeb0bae12de5
Munyangeyo Elie <munyangeyoelie38@gmail.com>
2023-10-20 08:49:50 -0700

    Updates

--- builtins_shell_exit.c
+++ builtins_shell_exit.c
@@ -3,12 +3,18 @@
 /**
  * shell_exit - Exit the shell.
  * @args: Arguments.
- * 
+ *
  * Return: Nothing.
-*/
+ */
 void shell_exit(char **args)
 {
-      int status = 0;
+	int status = 0;
 
-      if (args[1] )
+	if (args[1] != NULL)
+	{
+		status = _atoi(args[1]);
+	}
+	free_tokens(args);
+	free_last_input();
+	exit(status);
 }
