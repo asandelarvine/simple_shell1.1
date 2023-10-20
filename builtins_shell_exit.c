d4f56f84197ba06933122d1ed79f10075f773d45
Larvine <asandelarvine@gmail.com>
2023-10-20 18:21:46 +0300

    indentation

---
+++ builtins_shell_exit.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+
+/**
+ * shell_exit - Exit the shell.
+ * @args: Arguments.
+ * 
+ * Return: Nothing.
+*/
+void shell_exit(char **args)
+{
+      int status = 0;
+
+      if (args[1] )
+}
